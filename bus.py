08b0832063f42151c4c582994fe362a7fbc54920
johanesgeryaty <gpjohan324@gmail.com>
2024-11-23 14:58:25 +0700

    juju

--- bus.py
+++ bus.py
@@ -1,13 +1,17 @@
-
 bandung_bus = 200000
 semarang_bus = 100000
 surabaya_bus = 150000
-# def bus (destinasi):
-#     titik_awal = input("Masukan titik awal anda [Bandung,Semarang,Surabaya] : ".lower);
-#     jumlah_tiket = int(input("Berapa tiket yang ingin anda beli : "))
-#     for i in range(jumlah_tiket):
-#         if titik_awal == "bandung":
-#             harga_awal = bandung_bus
-#             if destinasi == "bandung":
-#                 harga_tujuan = bandung_bus
-#             elif destinasi == 
+def bus (destinasi):
+    titik_awal = input("Masukan titik awal anda [Bandung,Semarang,Surabaya] : ".lower);
+    jumlah_tiket = int(input("Berapa tiket yang ingin anda beli : "))
+    for i in range(jumlah_tiket):
+        if titik_awal == "bandung":
+            harga_awal = bandung_bus
+            if destinasi == "bandung":
+                harga_tujuan = bandung_bus
+                harga_tiket = harga_awal + harga_tujuan
+            elif destinasi == "semarang":
+                harga_tujuan = semarang_bus
+            elif destinasi == "surabaya":
+                harga_tujuan = surabaya_bus
+        
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
+import bus as bis
 nama_pembeli= input("Masukan nama anda : ")
 destinasi = input("Tujuan Anda [Surabaya,Semarang,Bandung] : ".lower)
 pilihan = input("Mau pesan tiket bus atau hotel [Bus,Hotel] ? ".lower)
-# if pilihan == "bus":
+if pilihan == "bus":
