811b92eb5d0b2757dc6a6706e76e91c679b4db7f
johanesgeryaty <gpjohan324@gmail.com>
2024-11-23 14:49:34 +0700

    hahaha

---
+++ bus.py
@@ -0,0 +1,13 @@
+
+bandung_bus = 200000
+semarang_bus = 100000
+surabaya_bus = 150000
+# def bus (destinasi):
+#     titik_awal = input("Masukan titik awal anda [Bandung,Semarang,Surabaya] : ".lower);
+#     jumlah_tiket = int(input("Berapa tiket yang ingin anda beli : "))
+#     for i in range(jumlah_tiket):
+#         if titik_awal == "bandung":
+#             harga_awal = bandung_bus
+#             if destinasi == "bandung":
+#                 harga_tujuan = bandung_bus
+#             elif destinasi == 
--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-print("hello")
-print("JO (MOk)")
+
+nama_pembeli= input("Masukan nama anda : ")
+destinasi = input("Tujuan Anda [Surabaya,Semarang,Bandung] : ".lower)
+pilihan = input("Mau pesan tiket bus atau hotel [Bus,Hotel] ? ".lower)
+# if pilihan == "bus":
